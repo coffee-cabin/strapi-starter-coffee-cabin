0fcc57720d5a08d9c9099fafd94654ef051faf95
Monico Moreno <monico_moreno@collaborare.net>
2021-04-20 21:00:53 -0500

    Added the menu items for coffee cabinf

--- starter/routes/index.js
+++ starter/routes/index.js
@@ -4,7 +4,7 @@ let nodeFetch = require('node-fetch');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  let users = nodeFetch('http://localhost:1337/users')
+  let users = nodeFetch('http://localhost:1337/menu-items')
     .then((data) => {
       if (data.status !== 200){
         return null;
@@ -13,8 +13,8 @@ router.get('/', function(req, res, next) {
     })
     .then((data) => {
       res.render('index', {
-        title: 'Front End Component of strapi-starter-to-customize',
-        users: data
+        title: 'Coffee Cabin Starter',
+        menuItems: data
       });
     });
 });
